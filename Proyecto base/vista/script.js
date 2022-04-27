da407f8cd1674fe5134d6771152f79578249678e
Jesus Ramirez Ayala <48115771+jesusraayala1298@users.noreply.github.com>
2022-04-26 21:37:31 -0500

    Ya quedo

--- Proyecto base/vista/script.js
+++ Proyecto base/vista/script.js
@@ -4,8 +4,8 @@ var divdatosRegistro = document.getElementById('datosRegistro');
 var divImagen = document.getElementById('divImagen');
 var nuevoUsuario = document.getElementById('nuevoUsuario');
 var viejoUsuario = document.getElementById('viejoUsuario');
-var txtSignIn= document.getElementById('txtSignIn');
-var txtSignUp= document.getElementById('txtSignUp');
+var txtSignIn= document.getElementById('SignIn');
+var txtSignUp= document.getElementById('SignUp');
 var click=true;
 
 function animacion() {
@@ -16,12 +16,16 @@ function animacion() {
             divdatosRegistro.classList.add('aparece');
             nuevoUsuario.classList.add('desaparece');
             viejoUsuario.classList.add('aparece');
+            txtSignIn.classList.add('aparece');
+            txtSignUp.classList.add('desaparece');
    
             divImagen.classList.remove('moverRegreso');
             divDatosInicio.classList.remove('aparece');
             divdatosRegistro.classList.remove('desaparece');
             nuevoUsuario.classList.remove('aparece');
             viejoUsuario.classList.remove('desaparece');
+            txtSignIn.classList.remove('desaparece');
+            txtSignUp.classList.remove('aparece');
         });
         click=false;
     }else if(click==false){
@@ -31,12 +35,16 @@ function animacion() {
             divdatosRegistro.classList.remove('aparece');
             nuevoUsuario.classList.remove('desaparece');
             viejoUsuario.classList.remove('aparece');
+            txtSignIn.classList.remove('aparece');
+            txtSignUp.classList.remove('desaparece');
 
             divImagen.classList.add('moverRegreso');
             divDatosInicio.classList.add('aparece');
             divdatosRegistro.classList.add('desaparece');
             nuevoUsuario.classList.add('aparece');
             viejoUsuario.classList.add('desaparece');
+            txtSignIn.classList.add('desaparece');
+            txtSignUp.classList.add('aparece');
         });
         click=true;
     }
